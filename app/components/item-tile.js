c01f1e9f4b136d873a95f003cfb53874eafac0da
Luca Quatela and Nick Lyman <luca+nick@>
2016-06-15 15:49:11 -0700

    add delete button for the items

--- app/components/item-tile.js
+++ app/components/item-tile.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
 
+  sortBy: ['rating:desc'],
+  sortedItemsByRating: Ember.computed.sort('items', 'sortBy'),
+
   actions: {
     addToCart(item) {
       this.get('shoppingCart').add(item);
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,12 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  
+  model() {
+    return this.store.findAll('item');
+  },
+
   actions: {
     saveNewItem3(params) {
       var newItem = this.store.createRecord('item', params);
       newItem.save();
       this.transitionTo('index');
     },
+    deleteItem(item){
+      return item.destroyRecord();
+    }
   }
 });
