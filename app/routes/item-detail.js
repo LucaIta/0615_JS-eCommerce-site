ab8bfe927af58135052725c179c84db184c38426
Luca Quatela and Nick Lyman <luca@>
2016-06-15 13:48:33 -0700

    Able to update item with integer rating.

--- app/routes/item-detail.js
+++ app/routes/item-detail.js
@@ -3,8 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     rateItem3(params, item) {
-      if(params[rating] !== null) {
-        item.set(rating, params[rating]);
+      if(params["rating"] !== null) {
+
+        // console.log(params[rating]);
+        item.set("rating", params["rating"]);
       }
     item.save();
     }
