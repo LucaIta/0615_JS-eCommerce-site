0effd63f79884f01083efd2f6adc440299485c8f
Luca Quatela and Nick Lyman <luca@>
2016-06-16 09:58:25 -0700

    Add total cost to shopping cart service and display correctly.

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -2,8 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   items: [],
+  total: 0,
 
   add(item){
+
     this.get('items').pushObject(item);
+
+    // this.set('total', item.get('cost')); // this works !!!!
+    this.set('total', this.get('total') + item.get('cost'));
+    console.log(this.get('total'));
+
   }
 });
